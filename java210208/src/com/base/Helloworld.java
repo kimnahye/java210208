9db7e789132ebed7a5a0a3a3e76f198f09f1d0f6
heyun <heyun@LAPTOP-8T6UQU5B>
2021-02-08 14:36:07 +0900

    이차 수정했습니다. 작성자 : 이순신

---
+++ java210208/src/com/base/Helloworld.java
@@ -0,0 +1,17 @@
+package com.base;
+
+public class Helloworld {
+	int age = 20;
+
+	public static void main(String[] args) {
+		Helloworld hw = new Helloworld();
+		hw.age = 31;
+		System.out.println("당신의 나이는" + hw.age+"입니다");
+		System.out.println("2차 수정부분입니다.");
+		
+		
+	
+
+	}
+
+}
